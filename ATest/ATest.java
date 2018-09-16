b38accf60d8c54194aa57f156ab11d7b84c794f5
blozh <1165884639@qq.com>
2018-09-16 08:43:46 +0800

    12

--- ATest/ATest.java
+++ ATest/ATest.java
@@ -1,14 +1,38 @@
-package ATest;
+import java.util.Scanner;
 
-class ATest {
-    public static void main(String [] args){
-        double[] a=new double[4];
-        int c=0;
-        for(int i=0;i<4;i++){
-            a[i]=i;
+public class ATest {
+        public static void main(String[] args)
+        {
+            Scanner sc=new Scanner(System.in);
+            //输入一个数据在新创建键盘录入对象之后
+            int number=sc.nextInt();
+            String s=zuoMiMa(number);
+            System.out.println("字符串为:"+s);
         }
-        for(double b:a){
-            System.out.println(b);
+
+        public static String zuoMiMa(int number)
+        {
+            int[] arr=new int[10];
+            int i=0,count=0;
+            while(number>0)
+            {
+                arr[i]=number%10;//得到的是最后一位数，因为要得到逆序输出
+                number/=10;
+                count++;
+                i++;
+            }
+            System.out.println("这是逆序输出：");
+            for(i=0;i<count;i++)
+            {
+                System.out.print(arr[i]+",");
+            }
+            System.out.println("$$$$$$$$$$");
+            String s="";
+            for(i=0;i<count;i++)
+            {
+                s+=arr[i];
+            }
+            return s ;
         }
-    }
+
 }
